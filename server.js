a72b70e9243bc1f8be5d0fef67665257b099f982
Ben Helmer <Atomox@gmail.com>
2017-11-22 13:43:58 -0500

    author: Atomox -- Minor cleanup and comments for the new endpoint.

--- server.js
+++ server.js
@@ -3,13 +3,16 @@ var app = express();
 var server = require('http').createServer(app);
 var status_debug = require('./mta.debug').debug;
 
-const mta_status_file = "mta_status";
-
 const mtaApi = require('./mta.api');
 const mtaStatus = require('./mta.status');
 
+// File where we'll store things. No extension, please.
+const mta_status_file = "mta_status";
+
+// How long before we refresh the feeds?
 const cacheMinutes = 15;
 
+// Which lines are worth our time?
 const testLines = [
  'ACE',
  'BDFM',
@@ -28,15 +31,15 @@ const testLines = [
 // Display a short summary of the current status on the console at startup.
 status_debug(mta_status_file, cacheMinutes, testLines);
 
-
+// Allow other domains to access us. (Prepare for mingling)
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
+
 app.get('/subway/status', function(req, resp, next) {
-  console.log('List time!');
 
 	// Load the data.
 	// Check the filesystem first.
@@ -49,29 +52,32 @@ app.get('/subway/status', function(req, resp, next) {
 				return Promise.reject('No data loaded from file or endpoint.');
 			}
 
+			// Basic structure of our response-to-be.
 			let stats = new Object;
 			stats.ok = [];
 			stats.ignored = [];
 			stats.line = {};
 
+			// Deal with all the nesting.
 			for (let o in data.service.subway[0]) {
 				for (let line in data.service.subway[0].line) {
 					let l = data.service.subway[0].line;
 
-					if (testLines && testLines.indexOf(l[line].name[0]) === -1) {
-						console.log(' <!> --- Bypassing Line: ', l[line].name[0]);
+					// Are we ignoring this line?
+					if (testLines.indexOf(l[line].name[0]) === -1) {
 						stats.ignored.push(l[line].name[0]);
 						continue;
 					} 
 
+					// Properly format our line status.
 					let text = mtaStatus.parseLineStatus(l[line].name[0], l[line].status[0], l[line].text);
 
-					
+					// Add the formatted line data to our results.
 					stats.line[l[line].name[0]] = text;
 				}
 			}
 
-
+			// JSON-ify and respond.
 			resp.json(stats);
 		});
 });
