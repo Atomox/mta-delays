004052bb4fe7a5c7fb9f756206aa1e208b96878d
Ben Helmer <Atomox@gmail.com>
2017-12-14 00:21:19 -0500

    author: Atomox -- Planned Dates Regex -- Improved tests and formula.

--- server/mta.status.xml.js
+++ server/mta.status.xml.js
@@ -297,63 +297,24 @@ function formatSingleStatusEvent(event) {
 function getMessageDateTime(text) {
 	let DatePattern = /(?=<span\s*class="DateStyle"\s*>(.*)<\/span>)/gi;
 	let dateResults = text.split(DatePattern);
-
-	if (dateResults[1]) {
-
-	}
-	else {
-		console.log('Could not find datetime:', text);
-	}
-
 	return (dateResults[1]) ? dateResults[1].trim() : null;
 }
 
 
 function getMessagePlannedWorkDate(text) {
-			
-/**
-	(Weekend|Weekends|Late Nights|Days|Late Evenings|All times)\s*,(\s*([0-9]{0,2}:?[0-9]{0,2}\s*[APM]{0,2}\s*)(Saturday|Sunday|and|Mon|Fri|Sat|Sun|to|\s)*){02}, ((Jan|Feb|Mar|Apr|May|June|July|Aug|Sept|Oct|Nov|Dec)\s*[0-9]{0,2}\s*-\s*[0-9]{0,2}\s*(\,|&bull\;)?\s*)*
-
-	(Weekend|Weekends|Late Nights|Days|Late Evenings|All times)\s*,(\s*([0-9]{0,2}:?[0-9]{0,2}\s*[APM]{0,2}\s*)*(Saturday|Sunday|and|Mon|Fri|Sat|Sun|to|\s|,)*)*, ((Jan|Feb|Mar|Apr|May|June|July|Aug|Sept|Oct|Nov|Dec|-|\s)\s*[0-9]{0,2}\s*-?\s*[0-9]{0,2}\s*(\,|&bull\;)?\s*)*
-*/
-	// Weekend, 9:45 PM Fri to 5 AM Mon, Nov 24 - 27
-	// Weekend, 10 PM Fri to 5 AM Mon, Nov 24 - 27
-	// Weekend, 3:45 AM Sat to 10 PM Sun, Nov 25 - 26
-	// Weekend , Saturday and Sunday, Nov 25 - 26
-	// Weekend , Saturday and Sunday , Nov 25 - 26
-	// Weekends, 11:15 PM Fri to 5 AM Mon, Nov 24 - 27 &bull; Dec 1 - 4
-	// Weekend, 7:30 AM to 7 PM, Saturday, Nov 25 9:30 AM to 7 PM, Sunday, Nov 26
-	// Until Summer 2018
-	// Days, 9 AM to 3 PM, Mon, Thu and Fri, Dec 4, 7 and 8 
-	// Days, 9 AM to 3 PM, Mon to Fri, Dec 11 - 15
-	// All times, Monday to Friday, Dec 4 - 8 Dec 11 - 15
-
-
-	// 11:15 PM Fri to 5 AM Mon
-
-	// Time to Time, Day
-	// 7:30 AM to 7 PM, Saturday
-
-	// Date Time to Time
-	// Nov 25 9:30 AM to 7 PM, Sunday, Nov 26
-
-
-	// let workDatePattern = /(Weekend|Weekends|Late Nights|Days|Late Evenings|All times|Until)\s*,(\s*([0-9]{0,2}:?[0-9]{0,2}\s*[APM]{0,2}\s*)(Saturday|Sunday|and|Mon|Fri|Sat|Sun|to|until|\s)*){02}, ((Jan|Feb|Mar|Apr|May|June|July|Aug|Sept|Oct|Nov|Dec|Spring|Summer|Fall|Winter)\s*[0-9]{0,2}\s*-\s*[0-9]{0,2}\s*(\,|&bull\;)?\s*)*/i;
 
 	// let workDatePattern = /((Weekend|Weekends|Late Nights|Days|Late Evenings|All times|Until)\s*,?(\s*([0-9]{0,2}:?[0-9]{0,2}\s*[APM]{0,2}\s*)(Saturday|Sunday|Monday|Tuesday|Wednesday|Thursday|Friday|Mon|Tue|Wed|Thur|Thu|Fri|Sat|Sun|to|until|and|\s)*,?){09},?\s((Jan|Feb|Mar|Apr|May|June|July|Aug|Sept|Oct|Nov|Dec|Spring|Summer|Fall|Winter)\s*[0-9]{0,2}\s*-?\s*[0-9]{0,2}\s*(20[0-9]{2})?\s*(\,|&bull\;)?\s*)*)+/i;
 
 	// In Progress -- Reduction
-	let workDatePattern = /((Weekends|Weekend|Late Nights|Days|Late Evenings|All times|Until)\s*,?(\s*((([0-9]{1,2}|[0-9]{1,2}:[0-9]{1,2})\s*(AM|PM)\s*)|([0-9]{1,2}\s*(-\s*[0-9]{1,2})?\s*(20[0-9]{2})?)?|(20[0-9]{2}))?\s*[,-]?\s*((Saturday|Sunday|Monday|Tuesday|Wednesday|Thursday|Friday|Sat|Sun|Mon|Tue|Wed|Thur|Thu|Fri|to|until|and)|(Jan|Feb|Mar|Apr|May|June|July|Aug|Sept|Oct|Nov|Dec|Spring|Summer|Fall|Winter))?\s*(\,|&bull\;)?\s*)*\s*)+/i;
+	let workDatePattern = /((Weekends|Weekend|Late Nights|Nights|Days|Late Evenings|Evenings|All times|Until)\s*,?(\s*((([0-9]{1,2}|[0-9]{1,2}:[0-9]{1,2})\s*(AM|PM)\s*)|([0-9]{1,2}\s*(-\s*[0-9]{1,2})?\s*(20[0-9]{2})?)?|(20[0-9]{2}))?\s*[,-]?\s*((Saturday|Sunday|Monday|Tuesday|Wednesday|Thursday|Friday|Sat|Sun|Mon|Tue|Wed|Thur|Thu|Fri|to|until|beginning(\sat)?|further\snotice|and)|(Jan|Feb|Mar|Apr|May|June|July|Aug|Sept|Oct|Nov|Dec|Spring|Summer|Fall|Winter))?\s*(\,|&bull\;)?\s*)*\s*)+/i;
 
 	let dateResults = text.match(workDatePattern);
 
 	if (dateResults && dateResults[0]) {
 		return dateResults[0].trim();
 	}
-	else {
-		console.log('Can\'t find event dates.');
-	}
 
+	console.warn('Can\'t find event dates.');
 	return null;
 }
 
@@ -408,7 +369,8 @@ function getMessageAction(text) {
 		'skip_stations': [
 			'trains skip',
 			'station closures',
-			],
+			'trains are bypassing',
+		],
 		'service_ends_early': [
 			'end early',
 			'Service ends early',
--- server/test/parsemessage.test.js
+++ server/test/parsemessage.test.js
@@ -49,7 +49,7 @@ describe('Parse Service Messages', function() {
 
 		it ('Should Parse basic Posted dates from service interruptions.', () => {
 			for (let x in interupt_msg) {
-				if (x === 0) { coninue; }
+				if (myObj[x].type === 'PlannedWork') { continue; }
 
 				let result = mtaStatus.getMessageDateTime(interupt_msg[x]);
 
@@ -60,6 +60,8 @@ describe('Parse Service Messages', function() {
 
 	describe('Parse Planned Work Dates', () => {
 
+// TRACK REPLACEMENT  [A] Trains replace the [S] Rockaway Park Shuttle Days, 9:30 AM to 4 PM, Mon to Fri, Dec 11 - 15    Dec 18 - 22    [A]  trains make all  [S]  stops between Broad Channel and Beach 116 St.
+
 		let status_dates = {
 			weekend: {
 				simple: [
@@ -68,10 +70,15 @@ describe('Parse Service Messages', function() {
 					'Weekend, 3:45 AM Sat to 10 PM Sun, Nov 25 - 26',
 					'Weekend , Saturday and Sunday, Nov 25 - 26',
 					'Weekend , Saturday and Sunday , Nov 25 - 26',
+					'Weekend, 12:15 AM Sat to 4:30 AM Mon, Nov 18 - 20',
+					'Weekend, 7 AM to 7 PM, Sat and Sun, Nov 18 - 19',
 				],
 				multiweekend: [
 					'Weekends, 11:15 PM Fri to 5 AM Mon, Nov 24 - 27 &bull; Dec 1 - 4',
-					'All Times Until Summer 2018',
+					'Weekend, 7:30 AM to 7 PM, Saturday, Nov 25 9:30 AM to 7 PM, Sunday, Nov 26',
+					'Weekend, 3:45 AM Sat to 10 PM Sun, Nov 25 - 26',
+					'Weekends, 11:30 PM Fri to 5 AM Mon, Nov 24 - 27  &bull;  Dec 1 - 4',
+					'Weekends, 11:15 PM Fri to 5 AM Mon, until Dec 18',
 				],
 				complex: [
 					'Weekend, 7:30 AM to 7 PM, Saturday, Nov 25 9:30 AM to 7 PM, Sunday, Nov 26',
@@ -79,13 +86,39 @@ describe('Parse Service Messages', function() {
 			},
 			weekdays: {
 				simple: [
+					// Days
 					'Days, 9 AM to 3 PM, Mon to Fri, Nov 27 - Dec 1',
+					'Days, 9 AM to 3 PM, Mon to Fri, Dec 11 - 15',
+
+					// Late Night
+					'Late Nights, 11 PM to 5 AM, Mon to Fri, Dec 4 - 8',
+					'Late Nights, 11 PM to 5 AM, Mon to Wed, Dec 4 - 6',
+					'Late Nights, beginning at 10 PM, Mon to Thu, Dec 4 - 7',
+					'Late Nights, 9:45 PM Tue to 5 AM Wed, Dec 5 - 6',
+					'Late Nights, 9:45 PM to 5 AM, Mon to Fri, until Dec 15',
+
+					// Evenings & Late Evenings
+					'Evenings, Mon to Thu, Dec 4 - 7',
+					'Evenings, 8:30 PM to 11:59 PM, Mon to Thu, Dec 4 - 7',
+					'Late Evenings, 11 PM to 11:59 PM, Mon to Thu, Dec 4 - 7',
+					'Late Evenings, beginning 10 PM, Mon to Thu, Dec 4 - 7',
 				],
-				multiweek: [],
+				multiweek: [
+					'All times, Monday to Friday, Dec 4 - 8    Dec 11 - 15',
+					'Days, 9:45 AM to 3:30 PM, Mon and Tue, Dec 11 - 12    Mon to Fri, Dec 18 - 22    Tue to Fri, Dec 26 - 29',
+					'Days, 9:30 AM to 4 PM, Mon to Fri, Dec 11 - 15    Dec 18 - 22',
+					'Late Nights, 9:45 PM to 5 AM, Mon to Fri, Dec 4 - 8    Dec 11 - 15',
+				],
 				complex: [],
 			},
 			longterm: {
-				simple: [],
+				simple: [
+					'Until Saturday, Jan 6, 2018',
+					'All Times Until Summer 2018',
+					'Until Spring 2018',
+					'Until further notice',
+					'Until 11:59 PM Sun, Nov 26',
+				],
 			}
 		};
 
@@ -115,6 +148,20 @@ describe('Parse Service Messages', function() {
 			for (let x in status_dates.weekend.complex) {
 				let result = mtaStatus.getMessagePlannedWorkDate(status_dates.weekend.complex[x]); 
 				assert.equal(status_dates.weekend.complex[x], result);
+			}
+		});
+
+		it ('Should Parse [multi-weekday] planned work dates.', function() {
+			for (let x in status_dates.weekdays.multiweek) {
+				let result = mtaStatus.getMessagePlannedWorkDate(status_dates.weekdays.multiweek[x]); 
+				assert.equal(status_dates.weekdays.multiweek[x], result);
+			}
+		});
+
+		it ('Should Parse long-term planned work dates.', function() {
+			for (let x in status_dates.longterm.simple) {
+				let result = mtaStatus.getMessagePlannedWorkDate(status_dates.longterm.simple[x]); 
+				assert.equal(status_dates.longterm.simple[x], result);
 			}
 		});
 
