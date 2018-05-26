b8da6fda895c924e60bbde1053879275d737eb30
Ben Helmer <Atomox@gmail.com>
2018-05-26 14:53:54 -0400

    author: bhelmer -- NO-TICKET -- PRE-RELEASE -- Bliss Street Pre-Release, Header complete.

--- react/app/components/Header/DateDisplay.js
+++ react/app/components/Header/DateDisplay.js
@@ -2,12 +2,12 @@ let React = require('react');
 let moment = require('moment');
 
 const DateDisplay = (props) => {
-  
+
 	return (
 		<div className="DateHeader">
 			<h5>{moment(props.age).format('dddd, MMMM Do')}</h5>
 			<h1>{moment(props.age).format('h:mm A')}</h1>
-			<h4><span className="station">Astoria/Ditmars</span> Release</h4>
+			<h4><span className="station">Bliss St</span> Pre-Release</h4>
 		</div>
 	);
 }
